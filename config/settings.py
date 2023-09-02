aefa1ab5037bf8d7f15503c0aabfd6b21078f3cf
mabrishami00 <mabrishami78@yahoo.com>
2023-09-02 11:38:01 +0330

    chore: add apps to setting

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "accounts.apps.AccountsConfig",
+    "interactions.apps.InteractionsConfig",
+    "songs.apps.SongsConfig",
 ]
 
 MIDDLEWARE = [
