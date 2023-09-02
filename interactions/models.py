bc04ccb921bb56c30482cfe003c6a9becfdeeeeb
mabrishami00 <mabrishami78@yahoo.com>
2023-09-02 11:47:19 +0330

    feat: add model of interactions

--- interactions/models.py
+++ interactions/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from accounts.models import User
+from songs.models import Song
 
-# Create your models here.
+
+class Playlist(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Like(models.Model):
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_confirmed = models.BooleanField(default=False)
