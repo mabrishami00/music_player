6f68680e4abc9f32b6b24d1e0c5a98dc3d46f6e5
mabrishami00 <mabrishami78@yahoo.com>
2023-09-02 11:46:59 +0330

    feat: add model of songs

--- songs/models.py
+++ songs/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from accounts.models import User, Artist, Band
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
 
 # Create your models here.
+class Song(models.Model):
+    name = models.CharField(max_length=50)
+    created = models.DateTimeField(auto_now_add=True)
+    cover_photo = models.ImageField(upload_to="/songs/cover_photos")
+    audio_file = models.FileField(upload_to="/songs/audio_files")
+    band = models.ForeignKey(Band, on_delete=models.CASCADE, null=True, blank=True)
+    genres = models.ManyToManyField(Genre)
