ddc69e43b6a521550a0a9fa34a7a024256ce19b3
mabrishami00 <mabrishami78@yahoo.com>
2023-09-02 15:10:57 +0330

    fix: fix circular import

--- songs/models.py
+++ songs/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from accounts.models import User, Artist, Band
+# from accounts.models import Band
 
 
 class Genre(models.Model):
@@ -9,7 +9,7 @@ class Genre(models.Model):
 class Song(models.Model):
     name = models.CharField(max_length=50)
     created = models.DateTimeField(auto_now_add=True)
-    cover_photo = models.ImageField(upload_to="/songs/cover_photos")
-    audio_file = models.FileField(upload_to="/songs/audio_files")
-    band = models.ForeignKey(Band, on_delete=models.CASCADE, null=True, blank=True)
+    cover_photo = models.ImageField(upload_to="songs/cover_photos")
+    audio_file = models.FileField(upload_to="songs/audio_files")
+    band = models.ForeignKey("accounts.Band", on_delete=models.CASCADE, null=True, blank=True)
     genres = models.ManyToManyField(Genre)
